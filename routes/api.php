1da1b843dc4c8ccebd248df55d65a942cfd71a7a
Monique-Silva <monique.silva-ximenes@le-campus-numerique.fr>
2024-02-07 15:56:29 +0100

    user authentification step-by-step with authcontroller and authrequest creation

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
-use App\Models\Category;
-use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -29,3 +28,6 @@ Route::apiResource('/products', ProductController::class);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [AuthController::class, 'auth']);
+Route::post('/logout', [AuthController::class, 'logout']);
